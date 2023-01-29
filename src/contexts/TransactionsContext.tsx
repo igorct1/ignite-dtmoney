fcd52ca2491a455e75c3d5108440e884ca514264
igorct1 <igorct77@gmail.com>
2023-01-29 00:43:41 -0300

    filtering transactions

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -11,6 +11,7 @@ interface Transaction {
 
 interface TransactionContextType {
   transactions: Transaction[];
+  fetchData: (query?: string) => Promise<void>;
 }
 
 export const TransactionsContext = createContext({} as TransactionContextType);
@@ -22,18 +23,24 @@ interface TransactionProviderProps {
 export function TransactionsProvider({ children }: TransactionProviderProps) {
   const [transactions, setTransactions] = React.useState<Transaction[]>([]);
 
-  React.useEffect(() => {
-    async function fetchData(url: string) {
-      const response = await fetch(url);
+  async function fetchData(query?: string) {
+    const url = new URL("http://localhost:3000/transactions");
 
-      const json = await response.json();
-      setTransactions(json);
+    if (query) {
+      url.searchParams.append("q", query);
     }
-    fetchData("http://localhost:3000/transactions");
+
+    const response = await fetch(url);
+    const json = await response.json();
+    setTransactions(json);
+  }
+
+  React.useEffect(() => {
+    fetchData();
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchData }}>
       {children}
     </TransactionsContext.Provider>
   );
